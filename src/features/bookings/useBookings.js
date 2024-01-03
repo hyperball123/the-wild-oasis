1050294d0031fef22634c6febd0c42b81deab061
Sufiyan Shaikh <sufiwork2001@gmail.com>
2024-01-03 23:21:39 +0530

    completed booking filtering part

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,12 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     isLoading,
     data: bookings,
     error,
-  } = useQuery({ queryKey: ["bookings"], queryFn: getBookings });
+  } = useQuery({
+    // queryKey works like a dependency array
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
+  });
 
   return { isLoading, error, bookings };
 }
