64aa7921e186bb57471318a6ecedc2b924ba5593
Sufiyan Shaikh <sufiwork2001@gmail.com>
2024-01-18 19:15:34 +0530

    ...

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("sufiy@example.com");
-  const [password, setPassword] = useState("sufiy@123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("@");
 
   const { login, isLoading } = useLogin();
 
